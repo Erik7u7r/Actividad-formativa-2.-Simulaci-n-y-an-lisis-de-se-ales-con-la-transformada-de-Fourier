e9b3d8eb240511d17d8933bdba2a0fe25f245eaf
Erik7u7r <pichulove11@gmail.com>
2026-02-24 22:05:25 -0600

    Escalon FFT.py

--- Escalon FFT.py
+++ Escalon FFT.py
@@ -1,21 +1,21 @@
->>> import numpy as np
-... import matplotlib.pyplot as plt
-... 
-... # Parámetros
-... fs = 1000
-... t = np.linspace(-1, 1, fs, endpoint=False)
-... 
-... # Escalón manual
-... u = np.where(t >= 0, 1, 0)
-... 
-... # FFT
-... U = np.fft.fft(u)
-... frecuencias = np.fft.fftfreq(len(u), 1/fs)
-... 
-... # Graficar frecuencias positivas
-... plt.plot(frecuencias[:fs//2], np.abs(U)[:fs//2])
-... plt.title("FFT - Función Escalón")
-... plt.xlabel("Frecuencia (Hz)")
-... plt.ylabel("Magnitud")
-... plt.grid()
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Parámetros
+fs = 1000
+t = np.linspace(-1, 1, fs, endpoint=False)
+
+# Escalón manual
+u = np.where(t >= 0, 1, 0)
+
+# FFT
+U = np.fft.fft(u)
+frecuencias = np.fft.fftfreq(len(u), 1/fs)
+
+# Graficar frecuencias positivas
+plt.plot(frecuencias[:fs//2], np.abs(U)[:fs//2])
+plt.title("FFT - Función Escalón")
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Magnitud")
+plt.grid()
+plt.show()
