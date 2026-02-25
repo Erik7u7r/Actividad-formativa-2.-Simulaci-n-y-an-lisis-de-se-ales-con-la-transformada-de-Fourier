bf509b4847abc537ec299236c0c04354f680d20f
Erik7u7r <pichulove11@gmail.com>
2026-02-24 22:03:43 -0600

    Escalon.py

--- Escalon.py
+++ Escalon.py
@@ -1,16 +1,16 @@
->>> import numpy as np
-... import matplotlib.pyplot as plt
-... 
-... # Tiempo
-... t = np.linspace(-5, 5, 1000)
-... 
-... # Función escalón
-... u = np.where(t >= 0, 1, 0)
-... 
-... # Gráfica
-... plt.plot(t, u)
-... plt.title("Función Escalón Unitario")
-... plt.xlabel("Tiempo")
-... plt.ylabel("Amplitud")
-... plt.grid()
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Tiempo
+t = np.linspace(-5, 5, 1000)
+
+# Función escalón
+u = np.where(t >= 0, 1, 0)
+
+# Gráfica
+plt.plot(t, u)
+plt.title("Función Escalón Unitario")
+plt.xlabel("Tiempo")
+plt.ylabel("Amplitud")
+plt.grid()
+plt.show()
